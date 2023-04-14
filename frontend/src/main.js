c2a57618e81596da4fedca4d3c83c66711007ea2
Tako <tako520cn@gmail.com>
2023-04-14 08:49:28 -0400

    working on migrating vue3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -21,3 +21,30 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+// import { createApp } from 'vue';
+// import App from './App.vue';
+// import router from './router';
+// import vuetify from './plugins/vuetify';
+
+// // You need to update these libraries to their Vue 3 compatible versions if available
+// // and follow the individual libraries' migration guides
+// import VueScrollProgress from 'vue-scroll-progress';
+// import Header from 'v-header';
+// import VueGoogleCharts from 'vue-google-charts';
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+// import VVisible from 'v-visible';
+
+// const app = createApp(App);
+
+// app.use(VVisible);
+// app.use(BootstrapVue);
+// app.use(IconsPlugin);
+// app.use(VueGoogleCharts);
+// app.use(VueScrollProgress);
+// app.use(Header);
+
+// app.use(router);
+// app.use(vuetify);
+
+// app.mount('#app');
