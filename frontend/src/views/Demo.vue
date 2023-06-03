d8daa8923de845e481454fa989a7bd17dbdbab84
Tako <tako520cn@gmail.com>
2023-06-03 16:09:53 -0400

    basic in vue3

--- frontend/src/views/Demo.vue
+++ frontend/src/views/Demo.vue
@@ -1,12 +1,15 @@
 <template>
   <MovementArrive />
+  <SteeringComponent/>
 </template>
 <script>
 import MovementArrive from '../components/Demo/MovementArrive.vue'
+import SteeringComponent from '../components/Demo/SteeringComponent.vue'
 export default {
   name: 'ProjectDemo',
   components: {
-    MovementArrive
+    MovementArrive,
+    SteeringComponent
   }
 }
 </script>
