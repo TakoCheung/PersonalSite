dcffab45275644a4f9d7c8fc77f91303e3bd80dc
Tako <tako520cn@gmail.com>
2023-06-04 16:47:03 -0400

    wander in vue3

--- frontend/src/views/Demo.vue
+++ frontend/src/views/Demo.vue
@@ -1,26 +1,44 @@
 <template>
-  <v-col width="480">
-    <v-combobox label="Model" v-model="value" :items="items" variant="underlined"/>
-    <MovementArrive v-if="value == 'Arrive'" />
-    <SteeringComponent v-if="value == 'Kinetic'" />
-    <v-tooltip activator="parent" location="top">
-      <p v-if="value == 'Arrive'">Arrive</p>
-      <p v-if="value == 'Kinetic'">Kinetic</p>
-    </v-tooltip>
-
-  </v-col>
+  <v-row >
+    <v-col cols="12" align="center" dense>
+      <v-combobox label="Model" v-model="value" :items="items" variant="underlined" />
+    </v-col>
+    <v-col align="center" dense>
+      <MovementArrive v-if="value == 'Arrive'" />
+      <SteeringComponent v-if="value == 'Kinetic'" />
+      <MovementWander v-if="value == 'Wander'" />
+    </v-col>
+    <!-- <v-col> -->
+      <v-tooltip activator="parent" location="bottom">
+        <div v-if="value == 'Arrive'">
+          <p>Please click anywhere on the canvas, and the boid will land and</p>
+          <p>stop where you just click.</p>
+        </div>
+        <div v-if="value == 'Kinetic'">
+          <p>Please click anywhere on the canvas. The boid will go to where</p>
+          <p>you just click and back to keep moving.</p>
+        </div>
+        <div v-if="value == 'Wander'">
+          <p>Click anywhere inside the canvas to turn on/off the visualization,</p>
+          <p>and press any key to change different way to wander.</p>
+        </div>
+      </v-tooltip>
+    <!-- </v-col> -->
+  </v-row>
 </template>
 <script>
 import MovementArrive from '../components/Demo/MovementArrive.vue'
 import SteeringComponent from '../components/Demo/SteeringComponent.vue'
+import MovementWander from '../components/Demo/MovementWander.vue'
 export default {
   name: 'ProjectDemo',
   components: {
     MovementArrive,
-    SteeringComponent
+    SteeringComponent,
+    MovementWander
   },
   data: () => ({
-    items: ['Kinetic', 'Arrive', 'fizz', 'buzz'],
+    items: ['Kinetic', 'Arrive', 'Wander', 'buzz'],
     value: 'Kinetic',
   }),
 
