e1f0ef6981e8105ab733c77b04ff1f1dc4caa32f
Tako <tako520cn@gmail.com>
2024-09-23 23:14:16 -0400

    resume added

--- frontend/src/views/Resume.vue
+++ frontend/src/views/Resume.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <PDFViewer source=../../assets/Certificate_9865283_08282024.pdf style="height: auto; width: auto" /> -->
+    <PDFViewer source=../../assets/KWAN_HO_TAKO_CHEUNG.pdf style="height: auto; width: auto" />
     <v-row >
       <v-col cols="12">
         <PDF src="../../assets/KWAN_HO_TAKO_CHEUNG.pdf" />
@@ -9,12 +9,13 @@
   </div>
 </template>
 <script>
-// import PDFViewer from 'pdf-viewer-vue'
+import PDFViewer from 'pdf-viewer-vue'
 import PDF from "pdf-vue3";
 export default {
   name: 'tako-resumes',
   components: {
     PDF,
+    PDFViewer
   },
 }
 </script>
